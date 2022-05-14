b60711dc21a372499738db896b2eda71c8e716aa
열일 <isawthat77@naver.com>
2022-05-14 16:18:39 +0900

    랜덤캣 api와 버튼 추가

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,21 +1,32 @@
 const TEMPLATE = '<input type="text">';
 
 export default class SearchInput {
-  constructor({ $target, onSearch }) {
-    const $searchInput = document.createElement("input");
-    this.$searchInput = $searchInput;
-    this.$searchInput.placeholder = "고양이를 검색해보세요.";
+  constructor({ $target, onSearch, onClick }) {
+    this.$target = $target
+    this.onSearch = onSearch
+    this.onClick = onClick
+    const $searchSection = document.createElement('section');
+    this.$searchSection = $searchSection
+    $searchSection.className = 'SearchSection'
+    $target.appendChild($searchSection)
 
-    $searchInput.className = "SearchInput";
-    $target.appendChild($searchInput);
+    this.render();
+  }
 
-    $searchInput.addEventListener("keyup", e => {
+  render() {
+    this.$searchSection.innerHTML =
+      `
+    <input class='SearchInput' placeholder='고양이를 검색해보세요.' autofocus/>
+    <button class='RandomButton'>🐱</button>
+    `
+    this.$searchSection.querySelector('.SearchInput').addEventListener("keyup", e => {
       if (e.keyCode === 13) {
-        onSearch(e.target.value);
+        this.onSearch(e.target.value);
       }
     });
 
-    console.log("SearchInput created.", this);
+    this.$searchSection.querySelector('.RandomButton').addEventListener('click', () =>{
+      this.onClick()
+    })
   }
-  render() {}
 }
--- src/api.js
+++ src/api.js
@@ -2,9 +2,23 @@ const API_ENDPOINT =
   "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 export const api = {
-  fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
-      res.json()
-    );
+  fetchCats: async(keyword) => {
+    try{
+      const res = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`)
+      if(!res.ok) throw new Error('에러 발생')
+      return await res.json()
+    } catch(e){
+      throw new Error(`다음과 같은 에러 발생 ${e.message}${e.status}`)
+    }
+  }, 
+
+  randomFetchCats : async() => {
+    try{
+      const res = await fetch(`${API_ENDPOINT}/api/cats/random50`)
+      if(!res.ok) throw new Error('에러 발생')
+      return await res.json()
+    } catch(e){
+      throw new Error(`다음과 같은 에러 발생 ${e.message}${e.status}`)
+    }
   }
 };
