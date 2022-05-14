1ceeb61f4b72a9017a0adfc26e96e5d61a6e0060
열일 <isawthat77@naver.com>
2022-05-14 15:58:46 +0900

    검색 결과가 없는 경우  UI 처리

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,47 +1,52 @@
 export default class SearchResult {
-    $searchResult = null;
-    data = null;
-    onClick = null;
-  
-    constructor({ $target, initialData, onClick }) {
-      this.$searchResult = document.createElement("div");
-      this.$searchResult.className = "SearchResult";
-      $target.appendChild(this.$searchResult);
-  
-      this.data = initialData;
-      this.onClick = onClick;
-  
-      this.render();
-    }
-  
-    setState(nextData) {
-      this.data = nextData;
-      this.render();
-    }
+  $searchResult = null;
+  data = null;
+  onClick = null;
 
+  constructor({ $target, initialData, onClick }) {
+    this.$searchResult = document.createElement("div");
+    this.$searchResult.className = "SearchResult";
+    $target.appendChild(this.$searchResult);
+    this.$inner
 
-  
-    render() {
-      if(this.data.isLoading){
-        this.$searchResult.innerHTML='Loading...'
-      } else {
-        this.$searchResult.innerHTML = this.data
-        .map(
-          cat => `
-            <div class="item">
-              <img src=${cat.url} alt=${cat.name} />
-            </div>
-          `
-        )
-        .join("");
-      }
+    this.data = initialData;
+    this.onClick = onClick;
 
-  
-      this.$searchResult.querySelectorAll(".item").forEach(($item, index) => {
-        $item.addEventListener("click", () => {
-          this.onClick(this.data[index]);
-        });
-      });
-    }
+    this.render();
   }
-  
+
+  setState(nextData) {
+    this.data = nextData;
+    this.render();
+  }
+
+
+
+  render() {
+    console.log(this.data)
+    if (this.data === null) {
+      this.$searchResult.innerHTML = '검색어 입력!'
+    } else {
+      if (this.data.isLoading) {
+        this.$searchResult.innerHTML = 'Loading...'
+      } else {
+        this.$searchResult.innerHTML =
+          this.data.length != 0 ?
+            this.data.map(
+              cat => `
+              <div class="item">
+                <img src=${cat.url} alt=${cat.name} />
+              </div>
+            `
+            )
+              .join("") : '검색된 결과가 없습니다.'
+      }
+    }
+
+    this.$searchResult.querySelectorAll(".item").forEach(($item, index) => {
+      $item.addEventListener("click", () => {
+        this.onClick(this.data[index]);
+      });
+    });
+  }
+}
