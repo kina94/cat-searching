26f761e718adf011592516100820cb54213aee5f
열일 <isawthat77@naver.com>
2022-05-14 15:08:26 +0900

    api export

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const API_ENDPOINT =
   "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
-const api = {
+export const api = {
   fetchCats: keyword => {
     return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
       res.json()
