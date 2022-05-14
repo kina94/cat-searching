7c22bba55fb2c6d792d20651631d7018020fcebd
열일 <isawthat77@naver.com>
2022-05-14 18:00:14 +0900

    최근 5개 검색 기록 렌더링

---
+++ src/SearchHistory.js
@@ -0,0 +1,33 @@
+export default class SearchHistory {
+    data = null
+    constructor({ $target, onClick }) {
+        this.$target = $target
+        this.$onClick = onClick
+        const $searchHistory = document.createElement('div')
+        this.$searchHistory = $searchHistory
+        $searchHistory.className = 'SearchHistory'
+        $target.appendChild($searchHistory)
+
+        this.render()
+    }
+
+    setState(nextData) {
+        this.data = nextData
+        this.render()
+    }
+
+    render() {
+        console.log(this.data)
+        if (this.data!=null) {
+            this.$searchHistory.innerHTML =
+                `
+        <ul>
+        ${this.data.map(history => {
+                    return `<li id=${history}>${history}</li>`
+                }).join('')
+                }
+        </ul>
+        `
+        }
+    }
+}
