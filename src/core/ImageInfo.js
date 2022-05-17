0e19d3e5d402fc7dc67bed3db4570ee1bdf41ed8
열일 <isawthat77@naver.com>
2022-05-17 21:37:02 +0900

    태그명 변경

--- src/core/ImageInfo.js
+++ src/core/ImageInfo.js
@@ -1,72 +1,73 @@
 export default class ImageInfo {
-    $imageInfo = null;
-    data = null;
-  
-    constructor({ $target, data, modalClose }) {
-      const $imageInfo = document.createElement("div");
-      $imageInfo.className = "image-info";
-      this.$imageInfo = $imageInfo;
-      $target.appendChild($imageInfo);
-  
-      this.data = data;
-      this.modalClose = modalClose;
-    
-      this.render();
-    }
-  
-    setState(nextData) {
-      this.data = nextData;
-      this.render();
-    }
+  $imageInfo = null;
+  data = null;
 
-    //모달 닫기
-    handleModalClose() {
-      this.$imageInfo.addEventListener('click', (e)=>{
-        try{
-          if(e.target.className==='image-info' || e.target.className==='close'){
-            this.modalClose()
-          }
-        } catch(e){
-          return
-        }
-      })
+  constructor({ $target, data, modalClose }) {
+    const $imageInfo = document.createElement("div");
+    $imageInfo.className = "image-info";
+    this.$imageInfo = $imageInfo;
+    $target.appendChild($imageInfo);
 
-      window.addEventListener('keydown', (e)=>{
-        if(e.key === 'Escape'){
+    this.data = data;
+    this.modalClose = modalClose;
+
+    this.render();
+  }
+
+  setState(nextData) {
+    this.data = nextData;
+    this.render();
+  }
+
+  //모달 닫기
+  handleModalClose() {
+    this.$imageInfo.addEventListener('click', (e) => {
+      try {
+        if (e.target.className === 'image-info' || e.target.className === 'close') {
           this.modalClose()
         }
-      })
-    }
-  
-    render() {
-      if(this.data.isLoading){
-        this.$imageInfo.innerHTML='Loading...'
-      } else {
-        if (this.data.visible) {
-          const { name, url, temperament, origin } = this.data.catInfo;
-    
-          this.$imageInfo.innerHTML = `
+      } catch (e) {
+        return
+      }
+    })
+
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        this.modalClose()
+      }
+    })
+  }
+
+  render() {
+    if (this.data.isLoading) {
+      this.$imageInfo.innerHTML = 'Loading...'
+    } else {
+      if (this.data.visible) {
+        const { name, url, temperament, origin } = this.data.catInfo;
+
+        this.$imageInfo.innerHTML = `
             <div class="content-wrapper">
               <div class="title">
                 <span>${name.split('/')[0]}</span>
-                <div class="close">x</div>
+                <button class="close">❌</button>
               </div>
+              <figure>
               <img src="${url}" alt="${name}"/>        
-              <div class="description">
-                <div>성격: ${temperament}</div>
-                <div>태생: ${origin}</div>
-              </div>
+              <figcaption class="description">
+                <p>성격: ${temperament}</p>
+                <p>태생: ${origin}</p>
+              </figcaption>
+              </figure>
             </div>`;
-          this.$imageInfo.style.display = "block";
-        } else {
-          this.$imageInfo.style.display = "none";
-        }
+        this.$imageInfo.style.display = "block";
+      } else {
+        this.$imageInfo.style.display = "none";
       }
-
-
-      this.handleModalClose()
     }
 
 
+    this.handleModalClose()
   }
-  
+
+
+}
